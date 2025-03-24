3bc90a1f946b80f1bd623c5b46d6dfe9c9aae3a9
IGNP Andre Pratama Milano <andremilano16@gmail.com>
2025-03-24 14:31:05 +0700

    menambah AboutScreen composable

---
+++ app/src/main/java/com/andre0016/taxcalculator/ui/screen/AboutScreen.kt
@@ -0,0 +1,11 @@
+package com.andre0016.taxcalculator.ui.screen
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AboutScreen() {
+    Text(
+        text = "about screen"
+    )
+}
