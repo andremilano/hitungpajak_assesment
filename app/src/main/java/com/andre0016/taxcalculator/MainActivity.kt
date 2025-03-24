2b91c1d07f8f1a9d2e7318bc12c82da3419bc1e1
IGNP Andre Pratama Milano <andremilano16@gmail.com>
2025-03-24 14:03:05 +0700

    first commit

---
+++ app/src/main/java/com/andre0016/taxcalculator/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.andre0016.taxcalculator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.andre0016.taxcalculator.ui.screen.MainScreen
+import com.andre0016.taxcalculator.ui.theme.TaxCalculatorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TaxCalculatorTheme {
+                MainScreen()
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TaxCalculatorTheme {
+        MainScreen()
+    }
+}
---
+++ app/src/main/java/com/andre0016/taxcalculator/ui/screen/MainScreen.kt
@@ -0,0 +1,54 @@
+package com.andre0016.taxcalculator.ui.screen
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.andre0016.taxcalculator.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = stringResource(R.string.app_name))
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer
+                ),
+                actions = {
+                    IconButton(onClick = {}) {
+                        Icon(
+                            imageVector = Icons.Outlined.Info,
+                            contentDescription = stringResource(R.string.tentang_aplikasi),
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        TaxCalculatorApp(modifier = Modifier.padding(innerPadding).padding(16.dp))
+    }
+}
+
+@Composable
+fun TaxCalculatorApp(modifier: Modifier = Modifier) {
+    Text(
+        text = "Pajak penghasilan",
+        modifier = modifier.padding(16.dp)
+    )
+}
