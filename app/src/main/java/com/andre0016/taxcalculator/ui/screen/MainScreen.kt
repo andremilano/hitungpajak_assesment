ba3eb6ad7e705e0a3a4809121c282f5f10b59e3a
IGNP Andre Pratama Milano <andremilano16@gmail.com>
2025-03-25 13:54:33 +0700

    menambah gambar latar belakang, memperbarui logika perhitungan pajak, dan menambah tombol reset serta validasi input

--- app/src/main/java/com/andre0016/taxcalculator/ui/screen/MainScreen.kt
+++ app/src/main/java/com/andre0016/taxcalculator/ui/screen/MainScreen.kt
@@ -1,5 +1,8 @@
 package com.andre0016.taxcalculator.ui.screen
 
+import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -31,6 +34,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
@@ -77,79 +84,114 @@ fun TaxCalculatorApp(modifier: Modifier = Modifier) {
     var selectedTaxType by rememberSaveable { mutableStateOf("Pajak Penghasilan (PPH)") }
     var selectedTaxRate by rememberSaveable { mutableStateOf("5%") }
     var taxResult by rememberSaveable { mutableDoubleStateOf(0.0) }
+    val context = LocalContext.current
 
     val taxTypes = listOf("Pajak Penghasilan (PPH)", "Pajak Pertambahan Nilai (PPN)")
     val taxRates = listOf("5%", "15%", "25%", "30%")
 
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .padding(16.dp)
-            .verticalScroll(rememberScrollState())
+    Box(
+        modifier = modifier.fillMaxSize()
     ) {
-        // Input Penghasilan
-        OutlinedTextField(
-            value = income,
-            onValueChange = { income = it },
-            label = {
-                Text(
-                    text = stringResource(R.string.placeholder),
-                    style = MaterialTheme.typography.headlineSmall
-                )},
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-            modifier = Modifier.fillMaxWidth()
+        // Background Image
+        Image(
+            painter = painterResource(id = R.drawable.pajak),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxSize().graphicsLayer { alpha = 0.5f },
+            contentScale = ContentScale.Fit
         )
 
-        Spacer(modifier = Modifier.height(16.dp))
-
-        // Dropdown Jenis Pajak
-        DropdownSelector(
-            label = stringResource(R.string.pajak_type),
-            options = taxTypes,
-            selectedOption = selectedTaxType,
-            onOptionSelected = { selectedTaxType = it }
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        // Dropdown Tarif Pajak (hanya jika memilih PPH)
-        if (selectedTaxType == "Pajak Penghasilan (PPH)") {
-            DropdownSelector(
-                label = stringResource(R.string.pajak_percentage),
-                options = taxRates,
-                selectedOption = selectedTaxRate,
-                onOptionSelected = { selectedTaxRate = it }
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-        }
-
-        // Tombol Hitung Pajak
-        Button(
-            onClick = {
-                val incomeValue = income.toDoubleOrNull() ?: 0.0
-                taxResult = when (selectedTaxType) {
-                    "Pajak Penghasilan (PPH)" -> calculatePPH(incomeValue, selectedTaxRate)
-                    "Pajak Pertambahan Nilai (PPN)" -> calculatePPN(incomeValue)
-                    else -> 0.0
-                }
-            },
-            modifier = Modifier.fillMaxWidth()
+        // Konten utama dengan latar belakang semi-transparan
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+                .verticalScroll(rememberScrollState()) // Supaya teks tetap jelas terbaca
+                .padding(16.dp)
         ) {
+            // Input Penghasilan
+            OutlinedTextField(
+                value = income,
+                onValueChange = { income = it },
+                label = {
+                    Text(
+                        text = stringResource(R.string.placeholder),
+                        style = MaterialTheme.typography.headlineSmall
+                    )
+                },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            // Dropdown Jenis Pajak
+            DropdownSelector(
+                label = stringResource(R.string.pajak_type),
+                options = taxTypes,
+                selectedOption = selectedTaxType,
+                onOptionSelected = { selectedTaxType = it }
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            // Dropdown Tarif Pajak (hanya jika memilih PPH)
+            if (selectedTaxType == "Pajak Penghasilan (PPH)") {
+                DropdownSelector(
+                    label = stringResource(R.string.pajak_percentage),
+                    options = taxRates,
+                    selectedOption = selectedTaxRate,
+                    onOptionSelected = { selectedTaxRate = it }
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+            }
+
+            // Tombol Hitung Pajak
+            Button(
+                onClick = {
+                    val incomeValue = income.toDoubleOrNull()
+                    if (incomeValue == null) {
+                        Toast.makeText(context, "Invalid input, harap masukkan angka yang valid!", Toast.LENGTH_SHORT).show()
+                    } else {
+                        taxResult = when (selectedTaxType) {
+                            "Pajak Penghasilan (PPH)" -> calculatePPH(incomeValue, selectedTaxRate)
+                            "Pajak Pertambahan Nilai (PPN)" -> calculatePPN(incomeValue)
+                            else -> 0.0
+                        }
+                    }
+                },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = stringResource(R.string.calculate_tax),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+//            reset button
+            Button(
+                onClick = {
+                    income = ""
+                    selectedTaxType = "Pajak Penghasilan (PPH)"
+                    selectedTaxRate = "5%"
+                    taxResult = 0.0
+                },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = stringResource(R.string.reset),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            // Hasil Pajak
             Text(
-                text = stringResource(R.string.calculate_tax),
-                style = MaterialTheme.typography.bodyMedium
+                text = stringResource(R.string.tax_result, taxResult),
+                style = MaterialTheme.typography.bodyLarge
             )
         }
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        // Hasil Pajak
-        Text(
-            text = "Pajak yang harus dibayar: Rp ${taxResult.toInt()}",
-            style = MaterialTheme.typography.bodyLarge
-        )
     }
 }
+
 
 // Composable untuk Dropdown
 @Composable
@@ -193,6 +235,25 @@ fun calculatePPH(income: Double, taxRate: String): Double {
 fun calculatePPN(income: Double): Double {
     return income * 0.11
 }
+
+//@Composable
+//fun IconPicker(isError: Boolean, unit: String) {
+//    if(isError){
+//        Icon(imageVector = Icons.Filled.Warning, contentDescription = "Error")
+//    } else {
+//        Text(text = unit)
+//    }
+//}
+//
+//@Composable
+//fun ErrorHint(isError: Boolean) {
+//    if(isError)
+//        Text(
+//            text = stringResource(id = R.string.input_invalid),
+//            style = MaterialTheme.typography.bodySmall,
+//            color = Color.Red
+//        )
+//}
 
 @Preview(showBackground = true)
 @Composable
